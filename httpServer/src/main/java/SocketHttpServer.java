47fd588e9090ec69a65c816151790c0037aaef48
songlijiang <190360080@qq.com>
2016-10-25 22:33:56 +0800

    change to nio

--- httpServer/src/main/java/SocketHttpServer.java
+++ httpServer/src/main/java/SocketHttpServer.java
@@ -1,9 +1,16 @@
 import org.apache.log4j.Logger;
 
-import java.io.*;
-import java.net.ServerSocket;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -17,21 +24,54 @@ public class SocketHttpServer {
 
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT);
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.socket().bind(new InetSocketAddress(DEFAULT_PORT));
+            serverSocketChannel.configureBlocking(false);
             logger.info("SocketHttpServer is start on "+DEFAULT_PORT);
+            Selector selector = Selector.open();
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            logger.info("register serverChannel "+serverSocketChannel.getLocalAddress());
 
-            while (true){
-              Socket socket =  serverSocket.accept();  //waiting new connection
-              BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                StringBuilder stringBuilder = new StringBuilder();
-               String line= bufferedReader.readLine();
-                while (line != null && !line.isEmpty()){
-                    logger.info(line);
-                    line = bufferedReader.readLine();
-                    stringBuilder.append(line).append("\r\n");
+            while (true) {
+                if(selector.select(500)==0){
+                    logger.info("timeout no connection");
+                    continue;
                 }
-                hander(socket,stringBuilder.toString());
-                socket.close();
+                Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
+                while (keyIterator.hasNext()) {
+                    SelectionKey selectionKey = keyIterator.next();
+                    if (selectionKey.isAcceptable()) {
+                        SocketChannel clientChannel = ((ServerSocketChannel) selectionKey.channel()).accept();
+                        if(clientChannel !=null){
+                            clientChannel.configureBlocking(false);
+                            clientChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(512));
+                            logger.info("register new clientChannel " + clientChannel.getRemoteAddress());
+                        }
+                    }
+
+                    if (selectionKey.isReadable()) {
+                        SocketChannel clientChannel = (SocketChannel) selectionKey.channel();
+                        ByteBuffer byteBuffer = (ByteBuffer) selectionKey.attachment();
+                        long read = clientChannel.read(byteBuffer);
+                        if (read == -1) {
+                            clientChannel.close();
+                        }
+                        if (read > 0) {
+                            logger.info("client write to server " + new String(byteBuffer.array()));
+                            selectionKey.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+                        }
+                    }
+
+                    if(selectionKey.isValid() && selectionKey.isWritable()){
+                        ByteBuffer byteBuffer = (ByteBuffer) selectionKey.attachment();
+                        byteBuffer.flip();
+                        SocketChannel clientChannel =(SocketChannel)selectionKey.channel();
+                        clientChannel.write(ByteBuffer.wrap(hander(new String(byteBuffer.array())).getBytes()));
+                        logger.info("server write to client "+new String(byteBuffer.array()));
+
+                        selectionKey.interestOps(SelectionKey.OP_READ);
+                    }
+                }
             }
 
         } catch (IOException e) {
@@ -39,23 +79,24 @@ public class SocketHttpServer {
         }
     }
 
-
-
-    public static void hander(Socket socket,String result){
+    public static String hander(String result){
         try {
-            OutputStream outputStream = socket.getOutputStream();
+            StringBuilder resultAll = new StringBuilder();
             List<String> headers = getHeader();
             for (String header : headers){
-                outputStream.write(header.getBytes());
-                outputStream.write("\r\n".getBytes());
+                resultAll.append(header);
+                resultAll.append("\r\n");
             }
-            outputStream.write("\r\n".getBytes());
-            outputStream.write(result.getBytes());
-            outputStream.write("\r\n".getBytes());
+            resultAll.append("\r\n");
+            resultAll.append(result);
+            resultAll.append("\r\n");
 
-            logger.info("response"+result);
-        } catch (IOException e) {
+            logger.info("response"+resultAll);
+            return resultAll.toString();
+        } catch (Exception e) {
+
             e.printStackTrace();
+            return e.toString();
         }
     }
 
